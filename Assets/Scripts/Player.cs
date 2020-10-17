91a99c4ee864e422179bca1dfcb4f2a3998f289e
GraemeMW <graememwilliams@gmail.com>
2020-10-16 23:30:04 -0700

    Ladders and Climbing! - Add ladder sprites / tilemap - Add climbing functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,20 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     const string COLLISION_LAYER_GROUND = "Ground";
+    const string COLLISION_LAYER_LADDER = "Ladder";
 
     // Tunables
-    [SerializeField] float playerSpeed = 1.0f;
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float moveSpeedInAir = 1.0f;
+    [SerializeField] float climbSpeed = 1.0f;
+    [SerializeField] float climbLateralVelocityThrottle = 0.1f;
+    [SerializeField] float climbDownVelocityFloor = -1.0f;
     [SerializeField] float jumpForce = 1.0f;
 
     // State
+    float playerSpeedDefault = 1.0f;
     bool isJumpPressed = false;
+    bool isClimbing = false;
     bool isAlive = true;
 
     // Cached references
@@ -28,11 +35,13 @@ public class Player : MonoBehaviour
         playerRigidbody2D = GetComponent<Rigidbody2D>();
         playerCollider2D = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+        playerSpeedDefault = moveSpeed;
     }
 
     private void Update()
     {
         GetUserInput();
+        SpeedCheck();
     }
 
     private void GetUserInput()
@@ -45,11 +54,25 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void SpeedCheck()
+    {
+        // Adjust speed if in air vs. on ground
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask(COLLISION_LAYER_GROUND)))
+        {
+            moveSpeed = moveSpeedInAir;
+        }
+        else
+        {
+            moveSpeed = playerSpeedDefault;
+        }
+    }
+
     private void FixedUpdate()
     {
         FlipPlayerLeftRight();
-        PlayerMove();
-        PlayerJump();
+        Move();
+        Jump();
+        ClimbLadder();
     }
 
     private void FlipPlayerLeftRight()
@@ -62,9 +85,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void PlayerMove()
+    private void Move()
     {
-        float deltaX = horizontal * Time.deltaTime * playerSpeed;
+        float deltaX = horizontal * Time.deltaTime * moveSpeed;
         if (!Mathf.Approximately(deltaX, 0f))
         {
             animator.SetBool("isRunning", true);
@@ -77,7 +100,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void PlayerJump()
+    private void Jump()
     {
         if (isJumpPressed)
         {
@@ -90,4 +113,59 @@ public class Player : MonoBehaviour
         }
 
     }
+
+    private void ClimbLadder()
+    {
+        if (playerCollider2D.IsTouchingLayers(LayerMask.GetMask(COLLISION_LAYER_LADDER)))
+        {
+            ClimbEntry();
+            float deltaY = vertical * Time.deltaTime * climbSpeed;
+            if (deltaY > 0)
+            {
+                ClimbUp(deltaY);
+            }
+            else
+            {
+                ClimbDown(deltaY);
+            }
+        }
+        else
+        {
+            animator.SetBool("isClimbing", false);
+            isClimbing = false;
+        }
+    }
+    private void ClimbEntry()
+    {
+        // Initial speed throttling on first collision to ladder (if in the air)
+        if (!isClimbing && !playerCollider2D.IsTouchingLayers(LayerMask.GetMask(COLLISION_LAYER_GROUND)))
+        {
+            playerRigidbody2D.velocity = climbLateralVelocityThrottle * playerRigidbody2D.velocity;
+        }
+
+        // Only do climbing animation if not touching ground
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask(COLLISION_LAYER_GROUND)))
+        {
+            animator.SetBool("isClimbing", true);
+        }
+        else
+        {
+            animator.SetBool("isClimbing", false);
+        }
+
+        isClimbing = true;
+    }
+
+    private void ClimbUp(float deltaY)
+    {
+        Vector2 velocityIncrement = new Vector2(0f, deltaY);
+        playerRigidbody2D.velocity += velocityIncrement;
+    }
+
+    private void ClimbDown(float deltaY)
+    {
+        Vector2 climbDownVelocity = new Vector2(playerRigidbody2D.velocity.x, Mathf.Clamp(playerRigidbody2D.velocity.y + deltaY, climbDownVelocityFloor, 0f));
+        playerRigidbody2D.velocity = climbDownVelocity;
+    }
+
 }
